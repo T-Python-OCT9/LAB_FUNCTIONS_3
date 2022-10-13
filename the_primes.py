1047cf21051491cd554de8c0e66de4be9ba03d6a
saud <Saudalotibi79@gmail.com>
2022-10-13 21:13:24 +0300

    add comments

--- the_primes.py
+++ the_primes.py
@@ -1,11 +1,15 @@
 def the_primes(num1 : int, num2: int) -> int:
-    for number in range(num1,num2+1):
-        count = 0
-        for i in range(1,number+1):
-            if number % i == 0:
-                count = count + 1
-        if count == 2: 
-            print(number)
+    ''' this function find the primes numbers '''
+
+    for number in range(num1,num2+1): # loop from the num1 to num2
+        count : int = 0 # define count to know how many time the number remainder was 0 
+        
+        for i in range(1,number+1): # loop from 1 to the number 
+            if number % i == 0: # chick if the remainder was 0
+                count = count + 1 # add 1 to count if the remainder was 0
+        
+        if count == 2: # chick if count is 2 after looping from 1 to the number
+            print(number) 
    
 the_primes(25,50)
 
